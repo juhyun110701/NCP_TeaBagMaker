449e9309778a1121910fdd938c84b4e66232d655
juhyun110701 <s2017s16@e-mirim.hs.kr>
2019-05-21 11:19:51 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TeaBagMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.combobox = new System.Windows.Forms.ComboBox();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // combobox
+            // 
+            this.combobox.FormattingEnabled = true;
+            this.combobox.Location = new System.Drawing.Point(12, 12);
+            this.combobox.Name = "combobox";
+            this.combobox.Size = new System.Drawing.Size(194, 20);
+            this.combobox.TabIndex = 0;
+            this.combobox.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // timelabel
+            // 
+            this.timelabel.AutoSize = true;
+            this.timelabel.Location = new System.Drawing.Point(12, 44);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(41, 12);
+            this.timelabel.TabIndex = 1;
+            this.timelabel.Text = "시간 : ";
+            this.timelabel.Click += new System.EventHandler(this.Timelabel_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 74);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(194, 52);
+            this.start.TabIndex = 2;
+            this.start.Text = "담그기!";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "2019/05/21 3612 이주현";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 174);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.combobox);
+            this.Name = "Form1";
+            this.Text = "TeaBagMaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combobox;
+        private System.Windows.Forms.Label timelabel;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.VisualBasic;
+
+namespace TeaBagMaker
+{
+    public partial class Form1 : Form
+    {
+        int CountNum = 0;
+
+        string[] tea = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+        
+        int[] times=new int[]{120,180,300,120};
+        int time = 0;
+   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tea.Length; i++)
+            {
+                this.combobox.Items.Add(tea[i]);
+                time = times[i];
+                if (combobox.Items.Count > 0)
+                {
+                    this.combobox.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                this.timelabel.Text = "시간 : " + time;
+        }
+
+        private void Timelabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Timer_Tick(sender,e);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            {
+                if (time < 1)
+                {
+                    this.timer.Enabled = false;
+                    this.timelabel.Text = "";
+                    MessageBox.Show("티백을 건지세요!", "알림", MessageBoxButtons.OK);
+                    this.combobox.Focus();
+                }
+                else
+                {
+                    time--;
+                    this.timelabel.Text = Convert.ToString(time / 60 + "분 " + time % 60 + "초 남았습니다!");
+                }
+          
+            }
+    }
+}
